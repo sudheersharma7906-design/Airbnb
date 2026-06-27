b180d3a2fa14fc4c30f325ab5e1b84ff767e279f
Sudheer Sharma <sudheersharma7906@gmail.com>
2026-06-27 15:03:14 +0530

    Use Gmail SMTP port 587 with STARTTLS to bypass Render port 465 block

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -7,7 +7,9 @@ const sendEmail = async ({ to, subject, html }) => {
 
     if (useGmail) {
       transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
         auth: {
           user: process.env.EMAIL,
           pass: process.env.EMAIL_PASSWORD,
